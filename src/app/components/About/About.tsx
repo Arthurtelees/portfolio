6a1fe1c5b6e5f7f0349bc4f064a65e238e080374
Arthurtelees <arthurnunesteles@gmail.com>
2026-05-27 11:17:45 -0300

    Novas fotos

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -321,8 +321,10 @@ export default function About() {
                     pauseOnHover={true}
                     cards={[
                       <img key={1} src="/images/photo-1.jpeg" alt="Arthur Teles" style={{ width: "100%", height: "100%", objectFit: "cover" }} />,
-                      <img key={2} src="/images/photo-2.jpeg" alt="Arthur Teles" style={{ width: "100%", height: "100%", objectFit: "cover" }} />,
+                      <img key={2} src="/images/photo-2.jpeg" alt="Arthur Teles" style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "top" }} />,
                       <img key={3} src="/images/photo-3.jpeg" alt="Arthur Teles" style={{ width: "100%", height: "100%", objectFit: "cover" }} />,
+                      <img key={4} src="/images/photo-4.jpeg" alt="Arthur Teles" style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "top" }} />,
+                      <img key={5} src="/images/photo-5.jpeg" alt="Arthur Teles" style={{ width: "100%", height: "100%", objectFit: "cover" }} />,
                     ]}
                   />
                 </div>
